e6195affb4ef8dbd1823425eb33f64c2d46928c0
robschwitzer <deadinthewater@me.com>
2018-08-07 15:38:01 +0100

    kata to find out who jon snows parents are

---
+++ kata13.js
@@ -0,0 +1,9 @@
+function jonSnowParents(dad, mom){
+  if (mom !== 'Lyanna Stark'|| dad !== 'Rhaegar Targaryen') {
+    return 'You know nothing, Jon Snow'
+  } else {
+    return 'Jon Snow you deserve the throne'
+  }
+}
+
+console.log(jonSnowParents('Rhaear Targaryen', 'Lyanna Stark'))
