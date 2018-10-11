f0924954c3065133d8c4464e373d89535b6d38f1
robschwitzer <deadinthewater@me.com>
2018-10-10 22:45:01 -0700

    kata that has something to do with game of thrones i think

---
+++ kata31.js
@@ -0,0 +1,26 @@
+let houses = [
+  {name: "Targaryen", motto: "Fire and Blood"},
+  {name: "Stark",     motto: "Winter is Coming"},
+  {name: "Bolton",    motto: "Our Blades Are Sharp"},
+  {name: "Greyjoy",   motto: "We Do Not Sow"},
+  {name: "Tully",     motto: "Family, Duty, Honor"},
+  {name: "Arryn",     motto: "As High as Honor"},
+  {name: "Lannister", motto: "Hear Me Roar!"},
+  {name: "Tyrell",    motto: "Growing Strong"},
+  {name: "Baratheon", motto: "Ours is the Fury"},
+  {name: "Martell",   motto: "Unbowed, Unbent, Unbroken"}
+]
+
+function motto(name) {
+  for (let i in houses) {
+    if (name === houses[i].name) {
+      return houses[i].motto
+    }
+    if (!name) {
+      return ''
+    }
+  }
+}
+
+
+console.log(motto('Stak'))
